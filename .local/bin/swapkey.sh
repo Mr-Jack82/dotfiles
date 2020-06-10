48f1f4bfd7d5aca36c640bb116668e95ca23d211
Vitaly <blackjack358@gmail.com>
2020-06-10 15:39:11 +0300

    Increase key speed via a rate change.
    Add a couple of comments so that the future I understand what’s what and why.

--- .local/bin/swapkey.sh
+++ .local/bin/swapkey.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+# Increase key speed via a rate change
+# idea from LukeSmithxyz
+xset r rate 300 50
+
+# Wait until GUI loads up
 sleep 4s #For KDE autostart
+
+# First of all, you need to swap somehow CapsLock and
+# Left_Control. Then that prog do all the stuff.
+# In particular when you press and hold Esc (or with
+# combinations with other keys) it works like Ctrl.
 xcape -e 'Control_L=Escape' &
