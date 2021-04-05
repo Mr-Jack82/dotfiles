03a28ad4b47668e436aa605422b4de49155ac2d4
Vitaly <blackjack358@gmail.com>
2021-04-05 21:58:04 +0300

    Moving to Lua half way
    Some of the interfaces (like `autocmd`, `augroup` and `map/nnoremap`)
    is not ready yet.

---
+++ .config/nvim/lua/config/nvim-hop/init.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_keymap('n', 's', '<Cmd>HopChar1<CR>', {silent = true})
+
+vim.cmd("hi HopNextKey guifg = #f7wf38")
+vim.cmd("hi HopNextKey1 guifg = #f7wf38")
+vim.cmd("hi HopNextKey2 guifg = #f7wf38")
