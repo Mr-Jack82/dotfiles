73775533cd6d80f05a9b777cc614912546ebb56a
Vitaly <blackjack358@gmail.com>
2021-04-18 02:05:30 +0300

    Add options file

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -126,5 +126,58 @@ vim.cmd('colorscheme gruvbox')
 -- For gruvbox specifically
 vim.o.background = "dark"
 
+-- Setting up font for Neovide
+vim.o.guifont="Hack Nerd Font Mono:h16"
+
 -- Make it more obvious where 'ColorColumn' is
 vim.cmd('highlight ColorColumn guibg=SlateBlue3')
+
+Theming = {
+    -- Press <space>fc to see all available themes
+    colorscheme = "edge",
+    --[[ Some colorscheme have multiple styles to choose from.
+      here are the available options:
+      For @gruvbox = medium, soft, hard
+      For @edge = default, aura, neon
+      For @sonokai = default, atlantis, andromeda, shusia, maia ]]
+    colorscheme_style = "aura",
+    -- Choose a stulusline:
+    -- Options: galaxy, airline, eviline, gruvbox, minimal, rounded
+    statusline = "eviline"
+}
+
+LSP = {
+    -- values: true, false
+    -- Enable or disable LSP globally
+    enabled = true,
+    -- Choose which servers to start automatically
+    bash = true,
+    clangd = true,
+    css = true,
+    emmet = true,
+    json = true,
+    lua = false,
+    python = true,
+    tsserver = true
+}
+
+Completion = {
+    -- values: true, false
+    -- Enable or disable completion globally
+    enabled = true,
+    items = 10,
+    -- Choose sources of completion
+    snippets = true,
+    lsp = true,
+    buffer = true,
+    path = true,
+    calc = true,
+    spell = true
+}
+
+Treesitter = {
+    -- Treesitter has big performance issues, choose to enable or not
+    enabled = true,
+    rainbow = true,
+    autotag = true
+}
